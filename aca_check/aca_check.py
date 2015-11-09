f8f7975562c236f792b8dd9b4de24a17fbf0e6ea
Tom Aldcroft <taldcroft@gmail.com>
2015-11-09 13:02:12 -0500

    WIP commit

--- aca_check/aca_check.py
+++ aca_check/aca_check.py
@@ -5,11 +5,17 @@ from __future__ import print_function, division, absolute_import
 import pyyaks.logger
 import parse_cm
 from Quaternion import Quat
+import Chandra.Maneuver
+from Chandra.Time import DateTime
 
 logger = pyyaks.logger.get_logger(name=__file__, level=pyyaks.logger.INFO,
                                   format="%(message)s")
 
 CMD_ACTION_CLASSES = []
+
+
+def as_date(time):
+    return DateTime(time).date
 
 
 class StateValue(object):
@@ -35,8 +41,13 @@ class StateValue(object):
 
 
 class SpacecraftState(object):
+    cmds = []
+    i_curr_cmd = None
     curr_cmd = None
     obsid = StateValue('obsid')
+    pitch = StateValue('pitch')
+    pcad_mode = StateValue('pcad_mode')
+    maneuver = StateValue('maneuver')
     q_att = StateValue('q_att', init_func=Quat)
     targ_q_att = StateValue('targ_q_att', init_func=Quat)
 
@@ -48,9 +59,46 @@ class SpacecraftState(object):
             return super(SpacecraftState, self).__getattribute__(attr)
 
     def iter_cmds(self):
-        for cmd in self.cmds:
+        self.cmds_to_add = []
+
+        for i, cmd in enumerate(self.cmds):
+            self.i_curr_cmd = i
             self.curr_cmd = cmd
             yield cmd
+
+        self.i_curr_cmd = None
+        self.curr_cmd = None
+
+        for cmd in self.cmds_to_add:
+            self.add_cmd(cmd)
+
+    def add_cmd(self, cmd):
+        """
+        Add command in correct order to the commands list.
+
+        TO DO: use scs and step for further sorting??
+        """
+        cmd_date = cmd['date']
+
+        if self.curr_cmd is None:
+            i_cmd0 = 0
+        else:
+            # If currently iterating through commands then only add command if
+            # it is *after* current command (otherwise iteration gets messed up).
+            # In this case add commands after iteration is done.
+            if cmd_date < self.curr_cmd['date']:
+                self.cmds_to_add.append(cmd)
+                return
+            i_cmd0 = self.i_curr_cmd + 1
+
+        cmds = self.cmds
+        for i_cmd in xrange(i_cmd0, len(self.cmds)):
+            if cmd_date < cmds[i_cmd]['date']:
+                self.cmds.insert(i_cmd, cmd)
+                break
+        else:
+            self.cmds.append(cmd)
+
 
 SC = SpacecraftState()
 
@@ -79,15 +127,40 @@ class CmdAction(object):
         return ok
 
 
-class ObsidCmd(CmdAction):
-    cmd_trigger = {'type': 'MP_OBSID'}
+class StateValueCmd(CmdAction):
+    """
+    Set a state value from a single key in the cmd dict.
 
+    Required class attributes:
+
+      - cmd_trigger
+      - state_name
+      - cmd_key (can also be a tuple of keys)
+    """
     @classmethod
     def action(cls, cmd):
-        SC.obsid = cmd['id']
+        if isinstance(cls.cmd_key, tuple):
+            value = tuple(cmd[key] for key in cls.cmd_key)
+        else:
+            value = cmd[cls.cmd_key]
+        setattr(SC, cls.state_name, value)
 
 
-class TargQAttCmd(CmdAction):
+class FixedStateValueCmd(CmdAction):
+    """
+    Base class for setting a single state value to something fixed.
+    These class attributes are required:
+
+      cmd_trigger = {}
+      state_name = None
+      state_value = None
+    """
+    @classmethod
+    def action(cls, cmd):
+        setattr(SC, cls.state_name, cls.state_value)
+
+
+class TargQAttCmd(StateValueCmd):
     """
     2009:033:01:18:19.704 |  8221758 0 | MP_TARGQUAT
     | TLMSID= AOUPTARQ, CMDS= 8,
@@ -96,11 +169,94 @@ class TargQAttCmd(CmdAction):
     """
     cmd_trigger = {'type': 'MP_TARGQUAT',
                    'tlmsid': 'AOUPTARQ'}
+    state_name = 'targ_q_att'
+    cmd_key = 'q1', 'q2', 'q3', 'q4'
+
+
+class QAttCmd(StateValueCmd):
+    """
+    Pseudo-command to update current attitude quaternion
+    """
+    cmd_trigger = {'tlmsid': 'update_q_att'}
+    state_name = 'q_att'
+    cmd_key = 'q1', 'q2', 'q3', 'q4'
+
+
+class ObsidCmd(StateValueCmd):
+    cmd_trigger = {'type': 'MP_OBSID', 'tlmsid': 'COAOSQID'}
+    state_name = 'obsid'
+    cmd_key = 'id'
+
+
+class PitchCmd(StateValueCmd):
+    """
+    Pseudo-command to update current Sun pitch angle.
+    """
+    cmd_trigger = {'tlmsid': 'update_pitch'}
+    state_name = 'pitch'
+    cmd_key = 'pitch'
+
+
+class ManeuverCmd(CmdAction):
+    """
+    Add a dict that records aggregate information about a maneuver.
+
+    This is an example of a delayed action since this command is injected
+    at maneuver start but evaluated maneuver end and so the obsid will be
+    correct.
+    """
+
+    cmd_trigger = {'tlmsid': 'add_maneuver'}
 
     @classmethod
     def action(cls, cmd):
-        targ_q_att = cmd['q1'], cmd['q2'], cmd['q3'], cmd['q4']
-        SC.targ_q_att = targ_q_att
+        maneuver = cmd['maneuver']
+        maneuver['final']['obsid'] = SC.obsid
+        SC.maneuver = cmd['maneuver']
+
+class StartManeuverCmd(CmdAction):
+    cmd_trigger = {'type': 'COMMAND_SW',
+                   'tlmsid': 'AOMANUVR'}
+
+    @classmethod
+    def action(cls, cmd):
+        atts = Chandra.Maneuver.attitudes(SC.q_att, SC.targ_q_att,
+                                          step=300, tstart=cmd['date'])
+        for time, q1, q2, q3, q4, pitch in atts:
+            date = DateTime(time).date
+            SC.add_cmd({'date': date,
+                        'tlmsid': 'update_q_att',
+                        'q1': q1, 'q2': q2, 'q3': q3, 'q4': q4})
+            SC.add_cmd({'date': date,
+                        'tlmsid': 'update_pitch',
+                        'pitch': pitch})
+
+        att0 = atts[0]
+        att1 = atts[-1]
+        maneuver = {'initial': {'date': as_date(att0['time']),
+                                'q_att': (att0['q1'], att0['q2'], att0['q3'], att0['q4']),
+                                'obsid': SC.obsid},
+                    'final': {'date': as_date(att1['time']),
+                              'q_att': (att1['q1'], att1['q2'], att1['q3'], att1['q4'])},
+                    'dur': att1['time'] - att0['time']}
+
+        SC.add_cmd({'date': maneuver['final']['date'],
+                    'tlmsid': 'add_maneuver',
+                    'maneuver': maneuver})
+
+
+class NMMMode(FixedStateValueCmd):
+    cmd_trigger = {'type': 'COMMAND_SW',
+                   'tlmsid': 'AONMMODE'}
+    state_name = 'pcad_mode'
+    state_value = 'NMAN'
+
+
+class NPNTMode(FixedStateValueCmd):
+    cmd_trigger = {'type': 'COMMAND_SW',
+                   'tlmsid': 'AONPMODE'}
+    state_name = 'pcad_mode'
+    state_value = 'NPNT'
 
 
 def set_initial_state():
@@ -115,7 +271,9 @@ def set_initial_state():
 set_initial_state()
 
 SC.cmds = parse_cm.read_backstop_as_list('test.backstop')
+SC.created_cmds = SC.cmds[:0]
 
+# Initial step of adding commands based on actual backstop commands
 for cmd in SC.iter_cmds():
     for cmd_action in CMD_ACTION_CLASSES:
         if cmd_action.trigger(cmd):
