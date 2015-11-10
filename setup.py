2aea703d995f8dc197744190cfb7238f562255ab
Tom Aldcroft <taldcroft@gmail.com>
2015-11-09 22:03:34 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hopper',
+      author='Tom Aldcroft',
+      description='Load checking package',
+      author_email='taldcroft@cfa.harvard.edu',
+      version='0.1',
+      zip_safe=False,
+      packages=['hopper'],
+      )
